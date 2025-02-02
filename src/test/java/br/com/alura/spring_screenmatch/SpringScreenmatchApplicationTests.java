52808745c41ae9bd9238a8d82e3bee677855e299
athena272 <guilhermera272@gmail.com>
2025-02-02 16:16:40 -0300

    add initial files

---
+++ src/test/java/br/com/alura/spring_screenmatch/SpringScreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.spring_screenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
