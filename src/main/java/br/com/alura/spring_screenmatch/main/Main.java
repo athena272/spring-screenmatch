70f56c675b230094226a23b0fcd2d6750beb5bd0
athena272 <guilhermera272@gmail.com>
2025-02-16 15:38:33 -0300

    Update Main.java

--- src/main/java/br/com/alura/spring_screenmatch/main/Main.java
+++ src/main/java/br/com/alura/spring_screenmatch/main/Main.java
@@ -1,12 +1,19 @@
 package br.com.alura.spring_screenmatch.main;
 
+import br.com.alura.spring_screenmatch.models.ConvertData;
+import br.com.alura.spring_screenmatch.models.EpisodeData;
+import br.com.alura.spring_screenmatch.models.SeasonData;
+import br.com.alura.spring_screenmatch.models.SerieData;
 import br.com.alura.spring_screenmatch.service.ApiConsumer;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    private Scanner input = new Scanner(System.in);
-    private ApiConsumer apiConsumer = new ApiConsumer();
+    private final Scanner input = new Scanner(System.in);
+    private final ApiConsumer apiConsumer = new ApiConsumer();
+    ConvertData convert = new ConvertData();
     private final String BASE_URL = "https://www.omdbapi.com/?t=";
     private final String API_KEY = System.getenv("API_KEY");
 
@@ -15,12 +22,23 @@ public class Main {
         System.out.println("Enter the name of the series for the search:");
         var seriesName = input.nextLine();
         var json = apiConsumer.fetchData(BASE_URL + seriesName.replace(" ", "+") + "&apikey=" + API_KEY);
+        SerieData serieData = convert.getData(json, SerieData.class);
+        System.out.println("JSON Response: " + serieData);
 
-        System.out.println("JSON Response: " + json);
-    }
+        List<SeasonData> seasons = new ArrayList<>();
+        for (int i = 1; i <= serieData.totalSeasons(); i++) {
+            json = apiConsumer.fetchData(BASE_URL + seriesName.replace(" ", "+") + "&season=" + i + "&apikey=" + API_KEY);
+            SeasonData seasonData = convert.getData(json, SeasonData.class);
+            seasons.add(seasonData);
+        }
+        //seasons.forEach(season -> System.out.println(season));
+        seasons.forEach(System.out::println);
 
-    public static void main(String[] args) {
-        Main mainApp = new Main();
-        mainApp.displayMenu();
+        for (int i = 0; i < serieData.totalSeasons(); i++) {
+            List<EpisodeData> episodesSeason = seasons.get(i).episodes();
+            for (EpisodeData episodeData : episodesSeason) {
+                System.out.println("Title episode: " + episodeData.title());
+            }
+        }
     }
 }
